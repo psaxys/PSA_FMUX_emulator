02fee3347f4e1ab428cda58cc55bf5d90e14004d
psaxys <148774981+psaxys@users.noreply.github.com>
2023-11-16 14:26:35 +0300

    Add files via upload
    fmux panel for psa car emulator.

---
+++ smeg_mux.ino
@@ -0,0 +1,49 @@
+#include <SPI.h>
+#include <mcp2515.h>
+
+struct can_frame canMsg1;
+struct can_frame canMsg2;
+MCP2515 mcp2515(10);
+
+ 
+void setup() {
+  //FMUX present
+  canMsg1.can_id  = 0x122;
+  canMsg1.can_dlc = 8;
+  canMsg1.data[0] = 0x00;
+  canMsg1.data[1] = 0x00;
+  canMsg1.data[2] = 0x00;
+  canMsg1.data[3] = 0x00;
+  canMsg1.data[4] = 0x00;
+  canMsg1.data[5] = 0x00;
+  canMsg1.data[6] = 0x00;
+  canMsg1.data[7] = 0x00;
+
+  //Кнопка "Меню"
+  canMsg2.can_id  = 0x122;
+  canMsg2.can_dlc = 8;
+  canMsg2.data[0] = 0x00;
+  canMsg2.data[1] = 0x40;
+  canMsg2.data[2] = 0x00;
+  canMsg2.data[3] = 0x00;
+  canMsg2.data[4] = 0x00;
+  canMsg2.data[5] = 0x00;
+  canMsg2.data[6] = 0x00;
+  canMsg2.data[7] = 0x00;
+
+  while (!Serial);
+  Serial.begin(115200);
+  
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_125KBPS, MCP_8MHZ);
+  mcp2515.setNormalMode();
+  
+  Serial.println("FMUX emulator");
+}
+
+void loop() {
+  mcp2515.sendMessage(&canMsg1);
+ // mcp2515.sendMessage(&canMsg2);
+  Serial.println("FMUX sent");
+  delay(200);
+}
